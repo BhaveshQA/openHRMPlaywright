330613bb8826a9a4e6ddefb23ad50c26afadd480
Bhavesh Rathod <hbhavesr@amazon.com>
2026-03-14 18:46:23 +0530

    added beforeEach hook

--- tests/orangeLogin.spec.js
+++ tests/orangeLogin.spec.js
@@ -2,18 +2,21 @@ import { test, expect } from '@playwright/test';
 import { LoginPage} from '../Pages/LoginPage'
 import {loginData} from '../testData/loginData.js'
 
-test("OrangeHRM User Login", async ({page}) =>{
-
+test.beforeEach("OrangeHRM User Login", async ({page}) =>{
     const loginpage = new LoginPage(page);
-
-
     await loginpage.navigateToLoginPage()
     await loginpage.login(loginData.username,loginData.password)
-    await loginpage.verifyLoginSuccess()
+});
+
+test("Verify dashboard element after login", async({page})=>{
+     const loginpage = new LoginPage(page);
+     await loginpage.verifyLoginSuccess()
+
+});
+   
     /**await page.goto('https://opensource-demo.orangehrmlive.com/')
     await page.fill('input[name="username"]','Admin')
     await page.fill('input[name="password"]','admin123')
     await page.click('button[type="submit"]');
     await expect(page.locator('h6:has-text("Dashboard")')).toBeVisible();
  **/
-})
